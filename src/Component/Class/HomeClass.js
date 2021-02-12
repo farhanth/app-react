5a0d199347070522feedcd74c5f81848aa5bf089
farhanth <farhantaufik98@gmail.com>
2021-02-12 20:52:38 +0700

    First Commit

---
+++ src/Component/Class/HomeClass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomeClass extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>This is Home with Class JS</h1>
+            </div>
+        )
+    }
+}
+
+export default HomeClass;
---
+++ src/Component/Functional/HomeFunction.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeFunction = () => {
+    return (
+        <div>
+            <h1>This is Home with Functional JSX</h1>
+        </div>
+    );
+}
+
+export default HomeFunction;
